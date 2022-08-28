50f000ea00f61d7688326e2239d1501c3e909fbf
vitoria <vitoriabccorrea@gmail.com>
2022-08-28 10:19:50 -0300

    Payment + Rest Template

---
+++ hr-payment/src/main/java/com/devsuperior/hrpayment/services/PaymentService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.hrpayment.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.devsuperior.hrpayment.entities.Payment;
+import com.devsuperior.hrpayment.entities.Worker;
+
+@Service
+public class PaymentService {
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public Payment getPayment(long workerId, int days) {
+		Map<String,String> uriVariables=new HashMap<>();
+		uriVariables.put("id",""+workerId);
+		Worker worker=restTemplate.getForObject(workerHost+"/workers/{id}",Worker.class,uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+	
+}
---
+++ hr-payment/src/test/java/com/devsuperior/hrpayment/HrPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
