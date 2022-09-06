12b7199d962bcb917948d005db8642a2aee7ea98
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-09-06 17:48:52 -0300

    USER AND ROLE entities done

---
+++ hr-user/src/main/java/com/hruser/entities/Role.java
@@ -0,0 +1,38 @@
+package com.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="tb_roles")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+	public Role() {
+	}
+	public Role(Long id, String roleName) {
+		this.id = id;
+		this.roleName = roleName;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
