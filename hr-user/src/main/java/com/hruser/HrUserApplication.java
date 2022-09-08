7a5ea5ed286a6856ebb84e0a2dc242d30a679200
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-09-08 17:30:39 -0300

    OAuth as a UserFeignClient

--- hr-user/src/main/java/com/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/hruser/HrUserApplication.java
@@ -8,19 +8,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication implements CommandLineRunner{
+public class HrUserApplication{
 
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("BCRYPT= "+ passwordEncoder.encode("123456"));
-		
-	}
-
 }
